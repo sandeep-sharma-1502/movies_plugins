0a9ca4e3c5b01dfb19f22e433b471681345dd1e0
Sandeep Sharma <sandeep08611@gmail.com>
2024-04-11 13:20:14 +0530

    "add setting"

--- movies.php
+++ movies.php
@@ -8,12 +8,82 @@
  * Author: Sandeep
  * Author URI: http://example.com
  * License: GPL-2.0+
- */         
+ * 
+ */
 
 
 add_action('init', 'wporg_custom_post_type');
 add_shortcode( 'movies', 'sada_shortcode' );
 add_action('wp_enqueue_scripts', 'enqueue_custom');
+
+function sada_shortcode($atts) {
+
+    $post_type = get_option('myplugin_settings_input_field1');
+
+    $posts_per_page = get_option('myplugin_settings_input_field2');
+
+    $author_name = get_option('myplugin_settings_input_field3');
+
+    $category_name = get_option('myplugin_settings_input_field4');
+
+    // Set the posts_per_page attribute in $atts
+    $atts['posts_per_page'] = $posts_per_page;
+
+    if($post_type != '') {
+        $atts['post_type'] = $post_type;
+    }
+    if($author_name != '') {
+        $atts['author_name'] = $author_name;
+    }
+
+    if($category_name != '') {
+        $atts['category_name'] = $category_name;
+    }
+
+    if($posts_per_page != '') {
+        $atts['posts_per_page'] = $posts_per_page;
+    }
+    
+
+    $atts = shortcode_atts(array(
+        'post_type' => 'wporg_product',
+        'posts_per_page' => '-1' ,
+        'author_name' => '',
+        'category_name' => '',  
+    ), $atts);
+
+
+    update_option('movies_shortcode_style', 'template2');
+
+    $query = new WP_Query($atts);
+
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+            global $post;
+            $post_id = $post->ID;
+            include 'template.php';
+        }
+    } else {
+        echo '<p>No posts found.</p>';
+    }
+    wp_reset_postdata();
+}
+
+function wporg_custom_post_type() {
+    register_post_type('wporg_product',
+        array(
+            'labels'      => array(
+                'name'          => __('Movies', 'textdomain'),
+                'singular_name' => __('Product', 'textdomain'),
+            ),
+                'public'      => true,
+                'has_archive' => true,
+                'supports' => array('title', 'editor', 'comments', 'revisions', 'trackbacks', 'author', 'excerpt', 'page-attributes', 'thumbnail', 'custom-fields', 'post-formats'),
+                'taxonomies' => array('category'),          
+        )
+    );
+}
 
 
 function enqueue_custom() {
@@ -26,63 +96,162 @@ function enqueue_custom() {
 }
 
 
+add_action('admin_menu', 'movies_plugin_settings_page');
 
- function wporg_custom_post_type() {
-	register_post_type('wporg_product',
-		array(
-			'labels'      => array(
-				'name'          => __('Movies', 'textdomain'),
-				'singular_name' => __('Product', 'textdomain'),
-			),
-				'public'      => true,
-				'has_archive' => true,
-                'supports' => array('title', 'editor', 'comments', 'revisions', 'trackbacks', 'author', 'excerpt', 'page-attributes', 'thumbnail', 'custom-fields', 'post-formats'),
-                'taxonomies' => array('category'),          
-		)
-	);
-}
-
-function sada_shortcode($atts) {
-    $wporg_atts = shortcode_atts(
-        array(
-            'post_type' => 'wporg_product',
-            'posts_per_page' => '5',
-            'author_name'=>'',
-            'category_name'=>'',
-            'template'=>'template1',
-        ), $atts
+function movies_plugin_settings_page() {
+    add_menu_page(
+        'Movies Settings', // Page title
+        'Settings', // Menu title
+        'manage_options', // Capability
+        'movies-settings', // Menu slug
+        'movies_settings_page_html', // Function to display the settings page
+        null, // Icon URL
+        99 // Position
     );
-
-
-update_option('movies_shortcode_style', $wporg_atts['template']);
-
-
-    // $args = array(
-    //     'post_type'  => 'wporg_product', 
-    //     'posts_per_page' => $wporg_atts['post'],
-    //     'author_name' => $wporg_atts['author'],
-    //     's' => $wporg_atts['category'],
-    // );
-
-
-    $custom_query = new WP_Query($wporg_atts);
-
-    echo '<div class="maincontaniner">';
-
-            if ($custom_query->have_posts()) {
-                while ($custom_query->have_posts()){
-                    
-                    $custom_query->the_post();
-                    $post_id = get_the_ID();
-                    include 'template.php';
-       
-                }
-                wp_reset_postdata(); 
-            }else{
-                    echo '<p>No books found</p>';
-            }
-
-  echo '</div>'; 
-
 }
+
+function movies_settings_page_html() {
+    ?>
+    <div class="wrap">
+        <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+
+        <form action="options.php" method="post">
+            <?php 
+                // security field
+                settings_fields( 'myplugin-settings-page' );
+
+                // output settings section here
+                do_settings_sections('myplugin-settings-page');
+
+                // save settings button
+                submit_button( 'Save Settings' );
+            ?>
+        </form>
+    </div>
+    <?php 
+}
+
+add_action('admin_init', 'movies_register_settings');
+
+function movies_register_settings() {
+    // Registration of settings not needed for this task
+
+        // Setup settings section
+        add_settings_section(
+            'myplugin_settings_section',
+            'Movies Settings Page',
+            '',
+            'myplugin-settings-page'
+        );
+    
+        // Registe input field1
+        register_setting(
+            'myplugin-settings-page',
+            'myplugin_settings_input_field1',
+            array(
+                'type' => 'string',
+                'sanitize_callback' => 'sanitize_text_field',
+                'default' => ''
+            )
+        );
+    
+        // Add text fields
+        add_settings_field(
+            'myplugin_settings_input_field1',
+            __( 'Post Type', 'my-plugin' ),
+            'myplugin_settings_input_field_callback1',
+            'myplugin-settings-page',
+            'myplugin_settings_section'
+        );
+        
+        // Registe input field2
+        register_setting(
+            'myplugin-settings-page',
+            'myplugin_settings_input_field2',
+            array(
+                'type' => 'string',
+                'sanitize_callback' => 'sanitize_text_field',
+                'default' => ''
+            )
+        );
+    
+        // Add text fields
+        add_settings_field(
+            'myplugin_settings_input_field2',
+            __( 'Posts Per Page', 'my-plugin' ),
+            'myplugin_settings_input_field_callback2',
+            'myplugin-settings-page',
+            'myplugin_settings_section'
+        );
+        
+        // Registe input field3
+        register_setting(
+            'myplugin-settings-page',
+            'myplugin_settings_input_field3',
+            array(
+                'type' => 'string',
+                'sanitize_callback' => 'sanitize_text_field',
+                'default' => ''
+            )
+        );
+    
+        // Add text fields
+        add_settings_field(
+            'myplugin_settings_input_field3',
+            __( 'Author Name', 'my-plugin' ),
+            'myplugin_settings_input_field_callback3',
+            'myplugin-settings-page',
+            'myplugin_settings_section'
+        );
+
+        register_setting(
+            'myplugin-settings-page',
+            'myplugin_settings_input_field4',
+            array(
+                'type' => 'string',
+                'sanitize_callback' => 'sanitize_text_field',
+                'default' => ''
+            )
+        );
+
+        add_settings_field(
+            'myplugin_settings_input_field4',
+            __( 'Category Name', 'my-plugin' ),
+            'myplugin_settings_input_field_callback4',
+            'myplugin-settings-page',
+            'myplugin_settings_section'
+        );
+
+      
+}
+
+function myplugin_settings_input_field_callback1() {
+    $myplugin_input_field = get_option('myplugin_settings_input_field1');
+    ?>
+    <input type="text" name="myplugin_settings_input_field1" class="regular-text" value="<?php echo isset($myplugin_input_field) ? esc_attr( $myplugin_input_field ) : ''; ?>" />
+    <?php 
+}
+
+function myplugin_settings_input_field_callback2() {
+    $myplugin_input_field = get_option('myplugin_settings_input_field2');
+    ?>
+    <input type="text" name="myplugin_settings_input_field2" class="regular-text" value="<?php echo isset($myplugin_input_field) ? esc_attr( $myplugin_input_field ) : ''; ?>" />
+    <?php 
+}
+
+function myplugin_settings_input_field_callback3() {
+    $myplugin_input_field = get_option('myplugin_settings_input_field3');
+    ?>
+    <input type="text" name="myplugin_settings_input_field3" class="regular-text" value="<?php echo isset($myplugin_input_field) ? esc_attr( $myplugin_input_field ) : ''; ?>" />
+    <?php 
+}
+
+function myplugin_settings_input_field_callback4() {
+    $myplugin_input_field = get_option('myplugin_settings_input_field4');
+    ?>
+    <input type="text" name="myplugin_settings_input_field4" class="regular-text" value="<?php echo isset($myplugin_input_field) ? esc_attr( $myplugin_input_field ) : ''; ?>" />
+    <?php 
+}
+
+
 ?>
